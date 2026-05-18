f13fb28c66d9e86a43af580d7c91849576366f49
Agniswari Guha <agniswari.g@sustaintel.com>
2026-05-18 22:51:37 +0530

    render upload

--- Program.cs
+++ Program.cs
@@ -13,14 +13,27 @@ builder.Services.AddSession();
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseSession();
 
+app.UseAuthorization();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider
+        .GetRequiredService<ApplicationDbContext>();
+
+    db.Database.Migrate();
+}
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Auth}/{action=Login}/{id?}");
+
 
 app.Run();
